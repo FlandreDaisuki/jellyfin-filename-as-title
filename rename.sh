632a4df4b39d43a2210bd8b7b3f30dc81b07a2da
FlandreDaisuki <vbnm123c+github@gmail.com>
2024-05-06 20:51:27 +0800

    fix: traverse folder after rename all videos

--- rename.sh
+++ rename.sh
@@ -4,7 +4,7 @@ TEMP_DIR="$(mktemp -d)"
 cd "${TEMP_DIR}" || exit
 
 repeat_str() {
-  head -c "${1:-0}" < /dev/zero | tr '\0' "${2:- }"
+  head -c "${1:-0}" </dev/zero | tr '\0' "${2:- }"
 }
 
 jellyfin() {
@@ -12,15 +12,15 @@ jellyfin() {
   curl -s -H "X-MediaBrowser-Token: ${API_TOKEN}" $@
 }
 
-jellyfin "${BASE_URI}/Users" > 'Users.json'
+jellyfin "${BASE_URI}/Users" >'Users.json'
 USER_ID="$(
   jq -r "$(
     printf '.[] | select(.Name=="%s").Id' "${USER_NAME}"
   )" 'Users.json'
 )"
 
-jellyfin "${BASE_URI}/Users/${USER_ID}/Items" > 'Items.json'
-jq -rc '.Items[] | {Id: .Id, Name: .Name}' 'Items.json' > 'Folders.jsonl'
+jellyfin "${BASE_URI}/Users/${USER_ID}/Items" >'Items.json'
+jq -rc '.Items[] | {Id: .Id, Name: .Name}' 'Items.json' >'Folders.jsonl'
 
 # shellcheck disable=SC3043
 traverse_folders() {
@@ -38,22 +38,17 @@ traverse_folders() {
     FOLDER_NAME="$(echo "${FOLDER_META}" | jq -r '.Name')"
     echo "$(repeat_str "${DEPTH}" '  ')- ${FOLDER_NAME}"
 
-    jellyfin "${BASE_URI}/Users/${USER_ID}/Items?Fields=Path&ParentId=${FOLDER_ID}" \
-      > "${FOLDER_ID}.json"
+    jellyfin "${BASE_URI}/Users/${USER_ID}/Items?Fields=Path&ParentId=${FOLDER_ID}" >"${FOLDER_ID}.json"
 
-    jq -rc '.Items[] | select(.IsFolder==false) | {Id: .Id, Name: .Name}' "${FOLDER_ID}.json" \
-      > "VideosIn${FOLDER_ID}.jsonl"
+    jq -rc '.Items[] | select(.IsFolder==false) | {Id: .Id, Name: .Name}' "${FOLDER_ID}.json" >"VideosIn${FOLDER_ID}.jsonl"
 
-    jq -rc '.Items[] | select(.IsFolder==true) | {Id: .Id, Name: .Name}' "${FOLDER_ID}.json" \
-      > "FoldersIn${FOLDER_ID}.jsonl"
-
-    traverse_folders "FoldersIn${FOLDER_ID}.jsonl" "$((DEPTH + 1))"
+    jq -rc '.Items[] | select(.IsFolder==true) | {Id: .Id, Name: .Name}' "${FOLDER_ID}.json" >"FoldersIn${FOLDER_ID}.jsonl"
 
     while read -r VIDEO_META; do
       VIDEO_ID="$(echo "${VIDEO_META}" | jq -r '.Id')"
       VIDEO_NAME="$(echo "${VIDEO_META}" | jq -r '.Name')"
 
-      jellyfin "${BASE_URI}/Users/${USER_ID}/Items/${VIDEO_ID}" > "Video-${VIDEO_ID}.json"
+      jellyfin "${BASE_URI}/Users/${USER_ID}/Items/${VIDEO_ID}" >"Video-${VIDEO_ID}.json"
 
       VIDEO_FILENAME="$(
         basename "$(jq -r '.Path' "Video-${VIDEO_ID}.json")"
@@ -70,9 +65,11 @@ traverse_folders() {
 
       echo "$(repeat_str "${DEPTH}" '  ')  v ${VIDEO_NAME}"
 
-    done < "VideosIn${FOLDER_ID}.jsonl"
+    done <"VideosIn${FOLDER_ID}.jsonl"
 
-  done < "${FOLDERS_JSONL}"
+    traverse_folders "FoldersIn${FOLDER_ID}.jsonl" "$((DEPTH + 1))"
+
+  done <"${FOLDERS_JSONL}"
 }
 
 traverse_folders 'Folders.jsonl'
